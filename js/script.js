67a9d8affb0495047e1c88b3ae1da1b23dd63660
Christina <89390143+Christinalulu@users.noreply.github.com>
2026-04-29 17:51:51 +0200

    did the project page  and adding small details

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+function show(pageId){
+
+    document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
+    document.querySelectorAll('.nav-btn').forEach(b => b.classList.remove('active'));
+     document.getElementById(pageId).classList.add('active');
+      document.getElementById('btn-' + pageId).classList.add('active');
+
+}
