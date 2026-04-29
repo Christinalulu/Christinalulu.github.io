c121d536d4c57275e1876be0abb16e6265433eae
Christina <89390143+Christinalulu@users.noreply.github.com>
2026-04-29 19:14:00 +0200

    updated html css js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-
-function show(pageId){
-
-    document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
-    document.querySelectorAll('.nav-btn').forEach(b => b.classList.remove('active'));
-     document.getElementById(pageId).classList.add('active');
-      document.getElementById('btn-' + pageId).classList.add('active');
-
+function show(pageId) {
+  document
+    .querySelectorAll(".page")
+    .forEach((p) => p.classList.remove("active"));
+  document
+    .querySelectorAll(".nav-btn")
+    .forEach((b) => b.classList.remove("active"));
+  document.getElementById(pageId).classList.add("active");
+  document.getElementById("btn-" + pageId).classList.add("active");
 }
